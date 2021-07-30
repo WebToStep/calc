77570edc681c1334576ad30a13a722fa19be9863
WebStep <webtostep@yandex.kz>
2021-07-30 14:30:58 +0500

    закончил урок №2

--- main.js
+++ main.js
@@ -1,26 +1,20 @@
-const money = 3000,
-      income = 'руб.',
-      addExpenses = ['майка', 'футболка', 'рубашка'],
-      deposit = 2, 
-      mission = money * deposit, 
-      period = '24 часа';
+const money = 30000,
+income = 'фриланс',
+addExpenses = 'интернет, такси, коммуналка',
+deposit = true, 
+mission = 2200000, 
+period = 12,
+budgetDay = money/30;
 
-      function EndPlus(){
-         if(deposit === 1){
-            return 'у';
-         }else if(deposit >= 2 && deposit <= 4){
-            return 'и';
-         }else{
-            return ' и т.д.';
-         }
-      }
+console.log('money: ', typeof money);
+console.log('income: ', typeof income);
+console.log('deposit: ', typeof deposit);
 
-let endStr = EndPlus();
+console.log('addExpenses: ', addExpenses.length);
 
-alert('Внимание!!! Акция действует ' + period);
-console.log('Hello World!');
-console.log(`
-купи ${deposit + ' ' + addExpenses[1].slice(0, -1)}${endStr} всего за ${money + income} 
-`);
-console.warn('Спасибо за урок "!"');
+console.log('“Период равен ' + period + ' месяцев”');
+console.log('“Цель заработать ' + mission + ' рублей/долларов/гривен/юани”');
 
+console.log('addExpenses: ', addExpenses.toLocaleLowerCase().split(','));
+
+console.log('budgetDay: ', budgetDay);
