53b2c61f043b3dde21575f6b139bb254d5c8e146
WebStep <webtostep@yandex.kz>
2021-08-05 00:14:46 +0500

    ref code

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
 'use strict';
 
-let money = numberHandler('Ваш месячный доход?'),
+// статьи обязательных расходов
+const expenses = [],
 income = 'фриланс',
-addExpenses = stringHandler('Перечислите возможные расходы за рассчитываемый период через запятую. (пример: "Квартплата, проездной, кредит")'),
-deposit = isDeposit(), 
+// deposit = isDeposit(), 
 mission = 2200000, 
-period = 12,
-// обязательные расходы
-expenses1 = stringHandler('Введите обязательную статью расходов?'),
-amount1 = numberHandler('Во сколько это обойдется?'),
-expenses2 = stringHandler('Введите обязательную статью расходов?'),
-amount2 = numberHandler('Во сколько это обойдется?'),
+period = 12;
 
-// чистая прибыль
-accumulatedMonth = getAccumulatedMonth(),
-// чистая прибыль в день
-budgetDay = Math.floor(accumulatedMonth/30),
+
 // определение статуса клиента
-getStatusIncome = function(){
+const getStatusIncome = () => {
    if(budgetDay > 1200){
       return('У вас высокий уровень дохода');
    }else if(budgetDay > 600 && budgetDay < 1200){
@@ -27,86 +19,123 @@ getStatusIncome = function(){
    } else if(budgetDay < 0){
       return('Что то пошло не так');
    }
-},
-showTypeOf = function(data){
-   console.log(data, typeof(data));
 };
 
-// Обработчик ввода числа
-function numberHandler(text){
-   let value = +prompt(text);
+// const showTypeOf = function(data){
+//    console.log(data, typeof(data));
+// };
 
-   if(null || value === 0){
-      confirm('Введите значение');
-      return numberHandler();
-   }else if(isNaN(parseFloat(value))){
-      confirm('Пожалуйста введите только цифры');
-      return numberHandler();
+// вызов модального окна
+const modal = (text, test) => {
+   let value = prompt(text);
+
+   // если value true и число
+   if(test(value) && !!parseFloat(value)){
+      console.log('value число: ', value);
+      return +value;
+   // если value true и строка
+   }else if(test(value) && !parseFloat(value)){
+      console.log('value строка: ', value);
+      return value;
    }else{
+      confirm('Введите верное значение');
+      modal(text, test);
+   }
+};
+
+
+// Проверка ввода числа
+const numberHandler = (value) => {
+   // console.log('приходит', value);
+   console.log('!isNaN число: ', !isNaN( parseFloat(value) && isFinite(value) ));
+   return !isNaN( parseFloat(value) && isFinite(value) );
+};
+
+// Проверка ввода строки
+const stringHandler = (value) => {
+   if(value === null || value === ''){
+      return false;
+   }else if(!Number.isNaN(parseFloat(value))){
+      return false;
+   }else{
+      return true;
+   }
+
+};
+
+// есть ли депозит?
+const isDeposit = () => {
+   let value = prompt('Есть ли у вас депозит в банке? (Введите Да или Нет)');
+
+   if(value === null || value === ''){
+      confirm('Введите значение');
+      return isDeposit();
+   }else if(!Number.isNaN(parseFloat(value))){
+      confirm('Пожалуйста введите верное значение');
+      return isDeposit();
+   }else if(value.toLowerCase() === 'нет'){
+      value = false;
       return value;
    }
-
-}
-// обработчик ввода строки
-function stringHandler(text){
-   let Value = prompt(text);
-
-   if(Value === null || Value === ''){
-      confirm('Введите значение');
-      return stringHandler();
-   }else if(!Number.isNaN(parseFloat(Value))){
-      confirm('Введите верное значение');
-      return stringHandler();
-   }else{
-      return Value;
-   }
-
-}
-// есть ли депозит?
-function isDeposit(){
-   let Value = prompt('Есть ли у вас депозит в банке? (Введите Да или Нет)');
-
-   if(Value === null || Value === ''){
-      confirm('Введите значение');
-      return isDeposit();
-   }else if(!Number.isNaN(parseFloat(Value))){
-      confirm('Пожалуйста введите верное значение');
-      return isDeposit();
-   }else if(Value.toLowerCase() === 'нет'){
-      Value = false;
-      return Value;
-   }
-   else if(Value.toLowerCase() === 'да'){
-      Value = true;
-      return Value;
+   else if(value.toLowerCase() === 'да'){
+      value = true;
+      return value;
    }else{
       confirm('Пожалуйста введите верное значение');
       return isDeposit();
    }
+};
+
+// расчет обязательные расходы (итого:)
+let getExpensesMonth = function(){
+   let sum = 0;
+   
+   for(let i = 0; i < 2; i++) {
+      expenses[i] = modal('Введите обязательную статью расходов?', stringHandler);
+      sum += modal('Во сколько это обойдется?', numberHandler);
+   }
+   console.log('sum: ', sum);
+   return sum;
+};
 
 
-}
-// обязательные расходы
-function getExpensesMonth(){
-   return amount1 + amount2;
-}
-// накопления за месяц
-function getAccumulatedMonth(){
-   return money - getExpensesMonth();
-}
+// доходы за месяц (через callback проверяем на цифры)
+let money = modal('Ваш месячный доход?', numberHandler),
+// расходы за месяц
+expensesAmount = getExpensesMonth();
+
+// расчет накопления за месяц
+const getAccumulatedMonth = () => {
+   return money - expensesAmount;
+};
+
+
+// чистая прибыль
+let accumulatedMonth = getAccumulatedMonth();
+// чистая прибыль в день
+let budgetDay = Math.floor(accumulatedMonth / 30);
+
 // период достижения цели
-function getTargetMonth(){
+const getTargetMonth = () => {
    let result = mission / accumulatedMonth;
    return Math.floor(result);
-}
+};
 
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
+// Строка слишком длинная незнаю как перенести так как это значение передается в функцию
+const addExpenses = modal(`
+   Перечислите возможные расходы за рассчитываемый период
+   через запятую. (пример: "Квартплата, проездной, кредит")
+   `, stringHandler).toLocaleLowerCase().split(',');
 
-console.log('Расходы за месяц вызов: ', getExpensesMonth());
-console.log('Вывод возможных расходов в виде массива: ', addExpenses.toLocaleLowerCase().split(','));
+// showTypeOf(money);
+// showTypeOf(income);
+// showTypeOf(deposit);
+console.log('Статьи расходов :', expenses);
+console.log('Итого доходы за месяц: ', money);
+console.log('Итого расходы за месяц: ', expensesAmount);
+console.log('Итого чистой прибыли за месяц: ', accumulatedMonth);
+console.log('Вывод возможных расходов в виде массива: ', addExpenses);
 console.log('Cрок достижения цели в месяцах: ', getTargetMonth());
 console.log('Бюджет на день: ', budgetDay);
-console.log('getStatusIncome(): ', getStatusIncome());
+console.log('Статус дохода: ', getStatusIncome());
 
